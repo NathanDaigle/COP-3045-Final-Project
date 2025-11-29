6ee29c57afc155b4cc27c3ae4eb082b5ba295384
Nathan Daigle <nathan.w.daigle@icloud.com>
2025-11-29 15:24:01 -0500

    Add Requirments

--- main.py
+++ main.py
@@ -6,14 +6,14 @@ import os
 import passwordManager as PM
 
 def main():
-    if not os.path.exists("Passwords.json"): # new user / missing file
+    if not os.path.exists("Passwords.json"): # New user / Missing file
         with open("Passwords.json", "x+")  as f:
             temp = {
                 "Master": secrets.token_urlsafe(64)
             }
             f.write(json.dumps(temp))
             
-    # ideally should never throw an error but try is always good
+    # Ideally should never throw an error but try is always good
     try:
         # format {"Master": "masterKey", "ServiceName": "HashedPassword"}
         currentPasswords = json.loads(open("Passwords.json", "r").read())
