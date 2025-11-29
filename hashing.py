db46d721b0ec0f9f12600f0fdd80905eb30e962a
Nathan Daigle <nathan.w.daigle@icloud.com>
2025-11-29 15:36:06 -0500

    Add functions

--- hashing.py
+++ hashing.py
@@ -1,5 +1,7 @@
 import argon2
 from argon2 import PasswordHasher
+from argon2.low_level import hash_secret
+import base64
 import json
 
 ph = PasswordHasher()
@@ -7,9 +9,31 @@ ph = PasswordHasher()
 def hash(password: str):
     return PasswordHasher().hash(password)
 
+def verifyPassword(password: str, hashed: str):
+    try:
+        ph.verify(hashed, password)
+        return True
+    except:
+        return False
+
 def dehash(password: str, hash: str):
     try:
         ph.verify(hash, password)
         return True
     except argon2.exceptions.VerifyMismatchError:
         return False
+    
+def derive_key(password: str, salt: bytes, hash_len=32):
+    time_cost = 2
+    memory_cost = 65536
+    parallelism = 8
+    key_bytes = hash_secret(
+        secret=password.encode(),
+        salt=salt,
+        time_cost=time_cost,
+        memory_cost=memory_cost,
+        parallelism=parallelism,
+        hash_len=hash_len,
+        type=hash_secret.Type.ID,
+    )
+    return base64.urlsafe_b64encode(key_bytes)
